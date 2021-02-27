7fa249f5239148358e4f12e662895653f50fda49
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2021-02-27 16:33:58 +0300

    Add route authentication for different admin roles

---
+++ middleware/checkAdminRolePermissions.js
@@ -0,0 +1,24 @@
+// Check if the admin with the given permissions can access this page
+// Redirect to /auth/login if there is an error
+
+const menus = [
+  {
+    role: 'product_head',
+    link: '/waitlist'
+  },
+  {
+    role: 'product_head',
+    link: '/questions'
+  },
+];
+
+module.exports = (req, res, next) => {
+  const admin = req.session.admin;
+  const route = '/' + req.originalUrl.split('/').map(each => each.split('?')[0]).filter(each => each.length)[0];
+  const menu = menus.find(menu => menu.link == route);
+
+  if (!menu || !admin.roles.includes(menu.role))
+    return res.redirect('/auth/login');
+
+  return next();
+}
